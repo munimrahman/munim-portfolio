86bf5d728072fa294ee444afa6647d1025546001
munimrahman <munimrh@gmail.com>
2023-05-01 02:20:49 +0600

    deploy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div className="hidden md:block max-w-[1115px] mx-auto pb-5 md:pb-0">
       <div className="divider mb-0"></div>
       <p className="text-sm text-center py-5 px-2">
-        Designed and built by <a className="text-primary">Munim Rahman</a> with{" "}
-        <span className="text-red-400">Love</span> &{" "}
+        Designed and built by{" "}
+        <Link
+          to={"https://web.facebook.com/munimrh/"}
+          target="_black"
+          className="text-sky-500"
+        >
+          Munim Rahman
+        </Link>{" "}
+        with <span className="text-red-400">Love</span> &{" "}
         <span className="text-[#6F4E37]">Coffee</span>
       </p>
     </div>
